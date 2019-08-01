8d8f1f4efdf5d0feeae2b6b3977b9766b3ed1c28
russa <aaron.russ@dfki.de>
2019-08-01 16:06:28 +0200

    migrated "manual" jsdoc3 generation to gulp-jsdoc3

--- doc/gulpfile.js
+++ doc/gulpfile.js
@@ -1,10 +1,31 @@
 
 var path = require('path');
+var fs = require('fs');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jsdoc = require('gulp-jsdoc3');
 var del = require('del');
-var exec = require('child_process').exec;
 var execFile = require('child_process').execFile;
+
+var outDir = './docs';
+var jaguarTemplateId = 'jaguarjs-jsdoc';
+var docstrapTemplateId = 'ink-docstrap';
+
+var getMmirPath = function(){
+	return path.dirname(require.resolve('mmir-lib'));
+};
+
+var getTemplatePath = function(templateId){
+	try {
+		return path.dirname(require.resolve(templateId));
+	} catch(err){
+		var pkgPath = path.normalize(__dirname+'/node_modules/'+templateId);
+		if(!fs.existsSync(pkgPath)){
+			throw new Error('Could not find template package for '+templateId);
+		}
+		return pkgPath;
+	}
+};
 
 var PROPERTIES_FILE = 'doc.properties';
 var propsLoader = require('properties');
@@ -38,53 +59,35 @@ var loadProperties = function(cb){
 };
 
 var cleanJsDoc = function(callback){
-	loadProperties(function(settings){
 
-		del([settings['dir.out.jsdoc'] + '/**/*']);
+	var outPath = path.normalize(outDir);
+	del([outPath + '/**/*']);
 
-		callback();
-		return gutil.noop();
-	});
+	callback();
 	return gutil.noop();
 };
 
 gulp.task('gen_jsdoc', function(callback) {
 
-	loadProperties(function(settings){
+	var config = JSON.parse(fs.readFileSync(path.resolve(__dirname+'/conf-jsdoc3.json')));
 
-		var args = [
-			settings['default.options.jsdoc.v3'],
-			'-c ' + path.normalize(settings['file.config.jsdoc.v3']),
-			'-d ' + path.normalize(settings['dir.out.jsdoc']),
-			'-t ' + path.normalize(settings['template.jsdoc.v3']),
-			'-R '  + path.normalize(settings['dir.src.in'] + '/../README.md'),
-			'-P '  + path.normalize(settings['dir.src.in'] + '/../package.json'),
-			path.normalize(settings['dir.src.in'])
-		];
+	var srcPath = getMmirPath();
+	var outPath = path.resolve(outDir);
 
-		var cmd = path.normalize(settings['exec.jsdoc.v3']) + ' ' + args.join(' ');
-		console.log('### '+cmd)
-		var child = exec(cmd, function(error, stdout, stderr){
+	var templateId = jaguarTemplateId;
+	var templatePath = getTemplatePath(templateId);
 
-			console.error(stderr);
-			if (error) {
-				callback(error);
-				return gutil.noop();
-			}
-			// console.log(stdout);
-			console.log('### wrote jsdoc3 files to '+path.normalize(settings['dir.out.jsdoc']));
+	var readmeFile = path.resolve(srcPath + '/../README.md');
+	var packageJsonFile = path.resolve(srcPath + '/../package.json');
 
-			callback();
-			return gutil.noop();
-		});
+	config.opts.destination = outPath;
+	config.opts.template = templatePath;
+	config.opts.readme = readmeFile;
+	config.opts.package = packageJsonFile;
 
-		//print conosle-output immediately as a way of progress-feedback (since jsdoc3 may take some time to process the files)
-		child.stdout.on('data', function(data){
-			console.log('	 ' + data.replace(/\r?\n$/, ''));
-		});
 
-	});
-
+	gulp.src([srcPath+'/*.js', srcPath+'/!(vendor)/**/*.js'], {read: false})
+        .pipe(jsdoc(config, callback));
 
 });
 
@@ -101,7 +104,7 @@ gulp.task('gen_depDoc', function(callback) {
 		];
 
 		var cwd = path.normalize(settings['dep.doc.working.dir']);
-		var child = execFile('node', args, {cwd: cwd} , function(error, stdout, stderr){
+		execFile('node', args, {cwd: cwd} , function(error, stdout, stderr){
 
 			console.error(stderr);
 			if (error) {
