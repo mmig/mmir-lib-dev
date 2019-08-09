7db8c23b83837a8a17869e015b13eec7d65cad2d
russa <aaron.russ@dfki.de>
2019-08-09 21:31:31 +0200

    added module description

--- template-parser/process-timestamps.js
+++ template-parser/process-timestamps.js
@@ -1,4 +1,9 @@
 
+/*
+ * HELPER remove timestamps that ANTLR3 compiler insertes into the compiled resources
+ *        -> for version control, so that recompiled sources should only "real" differences 
+ */
+
 var path = require('path');
 var removeTimestamps = require('./lib/remove-timestamps');
 
